a506daaf07ded0c9fe79a47b4ed74e8eda98c3d2
Baigson <talalmirza09@gmail.com>
2023-05-04 10:42:14 +0100

    sound

--- Assets/music handler.cs
+++ Assets/music handler.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
